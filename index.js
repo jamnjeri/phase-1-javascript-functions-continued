2ce437e68e768f429e0e74587f04acb730f21349
Jamila <jamnjeri@gmail.com>
2022-11-24 12:54:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+let activity;
+let work;
+
+//Saturday activity function
+function saturdayFun (activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+//Function expresiion
+const mondayWork = function (work = "go to the office"){
+    return (`This Monday, I will ${work}.`);
+}
+
+//Wrap Adjective
+
+function wrapAdjective(mark = "*") {
+    const part1 = "You are";
+    return function (message = "special") {
+        return (`${part1} ${mark}${message}${mark}!`);
+    };
+};
